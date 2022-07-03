c726d6b12532d3568df5a1a9ee52392508a65214
Ceylan Şentürk <ceng.ceysenturk@gmail.com>
2022-07-03 16:50:32 +0300

    Revert "hmw4"
    This reverts commit a90c3b12a2978581a04096c2a029277a8710a260.

--- isbasi-email-master/src/main/java/com/isbasi/Repository/ListenerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isbasi.Repository;
-
-import com.isbasi.dto.EmailDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ListenerRepository extends JpaRepository<EmailDto, Integer> {
-}
